b51770698f8ce9185d53a4b0398911eb085d6a35
LanaNabatova <lana.nabatova@gmail.com>
2017-12-08 10:03:12 +0200

    Base Page structure

--- wordpress/src/main/java/pages/BaseMenuMySite.java
+++ wordpress/src/main/java/pages/BaseMenuMySite.java
@@ -1,5 +1,30 @@
 package pages;
 
-public class BaseMenuMySite {
+import org.openqa.selenium.WebDriver;
 
+import webObject.WebObject;
+
+public class BaseMenuMySite extends BasePage{
+
+	 public BaseMenuMySite(WebDriver driver){
+	  super(driver);
+	 }
+	 
+	 WebObject viewSite = new WebObject("ViewSite", "//span[@class = 'menu-link-text'][text()='View Site']");
+	 WebObject stats = new WebObject("Stats", "//span[@class = 'menu-link-text'][text()='Stats']");
+	 WebObject plan = new WebObject("Plan","//span[@class = 'menu-link-text'][text()='Plan']");
+	 WebObject sitePages = new WebObject("SitePages","//span[@class = 'menu-link-text'][text()='Site Pages']");
+	 WebObject blogPosts = new WebObject("BlogPosts","//span[@class = 'menu-link-text'][text()='Blog Posts']");
+	 WebObject media = new WebObject("Media","//span[@class = 'menu-link-text'][text()='Media']");
+	 WebObject comments = new WebObject("Comments","//span[@class = 'menu-link-text'][text()='Comments']");
+	 WebObject themes = new WebObject("Themes","//span[@class = 'menu-link-text'][text()='Themes']");
+	 WebObject sharing = new WebObject("Sharing","//span[@class = 'menu-link-text'][text()='Sharing']");
+	 WebObject people = new WebObject("People","//span[@class = 'menu-link-text'][text()='People']");
+	 WebObject plugins = new WebObject("Plugins","//span[@class = 'menu-link-text'][text()='Plugins']");
+	 WebObject domains = new WebObject("Domains","//span[@class = 'menu-link-text'][text()='Domains']");
+	 WebObject settings = new WebObject("Settings","//span[@class = 'menu-link-text'][text()='Settings']");
+	 WebObject addSitePages = new WebObject("AddSitePages", "//li[2]/ul[1]/li[1]/a[2]");
+	 WebObject addBlogPosts = new WebObject("AddBlogPosts", "//li[2]/ul[1]/li[2]/a[2]");
+	 WebObject addNewSite = new WebObject("AddNewSite", "//a[@class='button my-sites-sidebar__add-new-site is-borderless']");
+	 
 }
