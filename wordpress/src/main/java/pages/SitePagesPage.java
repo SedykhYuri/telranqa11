4232a813eaa50448082dff54f0f7da3a93a8e2be
LanaNabatova <lana.nabatova@gmail.com>
2017-12-15 11:06:58 +0200

    Class PagesPage

---
+++ wordpress/src/main/java/pages/SitePagesPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import webObject.WebObject;
+
+public class SitePagesPage extends BasePage {
+
+	public SitePagesPage(WebDriver driver) {
+		super(driver);	
+	}
+	WebObject buttonPages = new WebObject ("button Pages",	"//a[@href='/pages/telranenglish.wordpress.com']");
+	
+	public  void firstArticle(){
+		System.out.println("1");
+		//ops.clickOnWebElement(buttonPages);
+		System.out.println("2");
+		String text =  driver.findElement(By.xpath("id(\"pages\")/div[2]/div[1]/a[1]")).getText();
+				                                                     //.getAttribute("textContent");
+//		String text2 = text.replaceAll("[^0-9]+", "");
+//	//System.out.println(text2);
+//	int qualityOnePlus =Integer.parseInt(text2);
+//	System.out.println("Found OnePlus Cell phones - "+qualityOnePlus);
+	System.out.println(text);
+	}
+
+}
